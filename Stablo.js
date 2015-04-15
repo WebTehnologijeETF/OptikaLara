d45b52443a87a4d502943c295e5a71d23e0cabb9
Larisa Besic <lbesic1@etf.unsa.ba>
2015-04-15 21:16:12 +0200

    Stablo

---
+++ Stablo.js
@@ -0,0 +1,116 @@
+﻿function stabloSuncane(){
+    if (document.getElementById("l1").style.display == "none") 
+    {
+        document.getElementById("l1").style.display = "block";
+        document.getElementById("ikonica1").src = "slike/ikonica_dolje.ico";
+    }
+   	else 
+    {
+   		document.getElementById("l1").style.display = "none";
+        document.getElementById("ikonica1").src = "slike/ikonica_desno.png";
+   	}
+}
+
+function stabloDioptrijske(){
+    if (document.getElementById("l2").style.display == "none") 
+    {
+        document.getElementById("l2").style.display = "block";
+        document.getElementById("ikonica2").src = "slike/ikonica_dolje.ico";
+    }
+   	else 
+    {
+   		document.getElementById("l2").style.display = "none";
+        document.getElementById("ikonica2").src = "slike/ikonica_desno.png";
+   	}
+}
+
+function stabloLece(){
+    if (document.getElementById("l3").style.display == "none") 
+    {
+        document.getElementById("l3").style.display = "block";
+        document.getElementById("ikonica8").src = "slike/ikonica_dolje.ico";
+    }
+   	else 
+    {
+   		document.getElementById("l3").style.display = "none";
+        document.getElementById("ikonica8").src = "slike/ikonica_desno.png";
+   	}
+}
+
+function stabloRayBan(){
+    if (document.getElementById("l11").style.display == "none") 
+    {
+        document.getElementById("l11").style.display = "block";
+        document.getElementById("ikonica").src = "slike/ikonica_dolje.ico";
+    }
+   	else 
+    {
+   		document.getElementById("l11").style.display = "none";
+        document.getElementById("ikonica").src = "slike/ikonica_desno.png";
+   	}
+}
+
+function stabloVogue(){
+    if (document.getElementById("l12").style.display == "none") 
+    {
+        document.getElementById("l12").style.display = "block";
+        document.getElementById("ikonica3").src = "slike/ikonica_dolje.ico";
+    }
+   	else 
+    {
+   		document.getElementById("l12").style.display = "none";
+        document.getElementById("ikonica3").src = "slike/ikonica_desno.png";
+   	}
+}
+
+function stabloDG(){
+    if (document.getElementById("l13").style.display == "none") 
+    {
+        document.getElementById("l13").style.display = "block";
+        document.getElementById("ikonica4").src = "slike/ikonica_dolje.ico";
+    }
+   	else 
+    {
+   		document.getElementById("l13").style.display = "none";
+        document.getElementById("ikonica4").src = "slike/ikonica_desno.png";
+   	}
+}
+
+function stabloDior(){
+    if (document.getElementById("l14").style.display == "none") 
+    {
+        document.getElementById("l14").style.display = "block";
+        document.getElementById("ikonica5").src = "slike/ikonica_dolje.ico";
+    }
+   	else 
+    {
+   		document.getElementById("l14").style.display = "none";
+        document.getElementById("ikonica5").src = "slike/ikonica_desno.png";
+   	}
+}
+
+function stabloCarrera(){
+    if (document.getElementById("l21").style.display == "none") 
+    {
+        document.getElementById("l21").style.display = "block";
+        document.getElementById("ikonica6").src = "slike/ikonica_dolje.ico";
+    }
+   	else 
+    {
+   		document.getElementById("l21").style.display = "none";
+        document.getElementById("ikonica6").src = "slike/ikonica_desno.png";
+   	}
+}
+
+function stabloGucci(){
+    if (document.getElementById("l22").style.display == "none") 
+    {
+        document.getElementById("l22").style.display = "block";
+        document.getElementById("ikonica7").src = "slike/ikonica_dolje.ico";
+    }
+   	else 
+    {
+   		document.getElementById("l22").style.display = "none";
+        document.getElementById("ikonica7").src = "slike/ikonica_desno.png";
+   	}
+}
